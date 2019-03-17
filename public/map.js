46ad2ad4cde74fe69975117fa08382ec198e2c39
Zhack Ariya <zhack@apptechgateway.com>
2019-03-18 05:19:18 +0800

    added yml

--- public/map.js
+++ public/map.js
@@ -4,9 +4,9 @@ function initMap() {
     var marker;
     let LATITUDE = 8.4861597
     let LONGITUDE = 124.6545985
-    if (document.getElementById('latitude').value){
-       LATITUDE = document.getElementById('latitude').value
-       LONGITUDE = document.getElementById('longitude').value
+    if (document.getElementById('latitude').value) {
+        LATITUDE = document.getElementById('latitude').value
+        LONGITUDE = document.getElementById('longitude').value
     }
     var map = new google.maps.Map(mapEl, {
         zoom: 20,
@@ -49,16 +49,16 @@ function initMap() {
 
         let timestamp = moment().format('YYYY-MM-DD hh:mm:ss');
         console.log(activityEntryTypeId);
-        switch(parseInt(activityEntryTypeId)){
+        switch (parseInt(activityEntryTypeId)) {
             case 11:
                 timestamp = moment('2019-03-17 08:23:22').format('YYYY-MM-DD hh:mm:ss');
-            break;
+                break;
             case 12:
                 timestamp = moment('2019-03-17 14:23:22').format('YYYY-MM-DD hh:mm:ss');
-            break;
+                break;
             case 13:
                 timestamp = moment('2019-03-17 23:23:22').format('YYYY-MM-DD hh:mm:ss');
-            break;
+                break;
         }
         // construct json
         let data = {
@@ -70,14 +70,43 @@ function initMap() {
             clientLongitude: lng,
             clientLatitude: lat
         }
-        let dataString = JSON.stringify(data,undefined,4);
+        let dataString = JSON.stringify(data, undefined, 4);
         console.log(dataString);
         textEl.value = dataString;
+        /**
+        // ajax call
+        let xhr = new XMLHttpRequest();
+        // Setup our listener to process completed requests
+        xhr.onload = function () {
 
+            // Process our return data
+            if (xhr.status >= 200 && xhr.status < 300) {
+                // What do when the request is successful
+                console.log('success!', xhr);
+            } else {
+                // What do when the request fails
+                console.log('The request failed!');
+            }
+
+            // Code that should run regardless of the request status
+            console.log('This always runs...');
+        };
+
+        // Create and send a POST request
+        // The first argument is the post type (GET, POST, PUT, DELETE, etc.)
+        // The second argument is the endpoint URL
+        
+        xhr.open('POST', 'http://localhost:8082/mobile/location');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBcHBUZWNoQVBJR2F0ZXdheSIsInVzZXJJZCI6IjEyMzQ1NjciLCJyb2xlIjoiYWRtaW4ifQ.i4YU9K8UZ2X1uAfXn2BMH8jUJ5zK_R4mjkJ2loSsy1_9Q7Wcmhitejl_93Kqw5QbZGNOhCZqHN6am5bUVC8ceA');
+
+        xhr.send(data);
+         */
     }
 }
 
-function copyToClipboard(textarea){
+function copyToClipboard(textarea) {
     this.textarea = textarea;
     this.textarea.focus(); this.textarea.select(); document.execCommand('copy');
 }
